cddefd95b548344d4682d7e7694aa49ebf72e9a8
Sarah ElShenawy <selshenawy69@gmail.com>
2025-09-24 17:17:20 +0300

    Merge products into master with unrelated histories

--- routes/cartrouter.js
+++ routes/cartrouter.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const {add_to_cart,showCart,edit_quantity_cart,delete_cart}=require('../controllers/cartcontroller')
-<<<<<<< HEAD
 const {authMiddleware,restrictTo }=require('../auth/auth')
 
 
@@ -9,15 +8,10 @@ router.post('/addcart',authMiddleware,restrictTo ("admin","user"), add_to_cart)
 router.get('/allcart',authMiddleware,restrictTo ("admin","user"), showCart)
 router.patch('/edit_cart/:id',authMiddleware,restrictTo ("admin","user"),edit_quantity_cart)
 router.delete('/edit_cart/:id',authMiddleware,restrictTo ("admin","user"), delete_cart)
-=======
 
 
 
-router.post('/addcart', add_to_cart)
-router.get('/allcart', showCart)
-router.patch('/edit_cart/:id',edit_quantity_cart)
-router.delete('/edit_cart/:id', delete_cart)
->>>>>>> products
+
 
 
 
--- routes/categoryrouter.js
+++ routes/categoryrouter.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const {add_category,allCategories}=require('../controllers/categorycontroller')
-<<<<<<< HEAD
 const {authMiddleware,restrictTo }=require('../auth/auth')
 
 router.post("/addCategory",authMiddleware,restrictTo ("admin"), add_category);
 router.get("/allCategories",authMiddleware,restrictTo ("admin","user"), allCategories);
-=======
-// const { isAdmin } = require("../middleware/auth");
-
-router.post("/addCategory", add_category);
-router.get("/allCategories", allCategories);
 
 
->>>>>>> products
+
 module.exports = router;
